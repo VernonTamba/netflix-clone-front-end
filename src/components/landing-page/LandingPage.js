b56bbf58a9c1ce9f0ea59478908dd2cbe81982d1
Vernon Tamba <tambzfantastico@gmail.com>
2022-05-25 13:31:37 +0700

    second commit

--- src/components/landing-page/LandingPage.js
+++ src/components/landing-page/LandingPage.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./LandingPage.css";
 import { Link } from "react-router-dom";
+import logo from "../../logo.svg";
 
 const LandingPage = () => {
   return (
     <div className="landingPage">
+      <img src={logo} className="App-logo" alt="logo" />
       <div className="landingPage__content">
         <h1>NETFLIX CLONE</h1>
+        <h3>(Front-End)</h3>
         <div className="landingPage__contentLink">
           <Link className="landingPage__links" to="/login">
             Login Page
--- src/components/main-page/Row.js
+++ src/components/main-page/Row.js
@@ -2,12 +2,15 @@ import React, { useEffect, useState, useRef } from "react";
 import "./Row.css";
 import axios from "../../axios";
 import { motion } from "framer-motion";
+import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 
 const Row = ({ title, fetchUrl, isPosterImage = false }) => {
   const baseUrl = "https://image.tmdb.org/t/p/original";
 
   const [movies, setMovies] = useState([{}]);
   const [width, setWidth] = useState(0);
+  const [visible, setVisible] = useState(0);
 
   const imagesRow = useRef();
 
@@ -23,12 +26,72 @@ const Row = ({ title, fetchUrl, isPosterImage = false }) => {
     fetchMovies();
   }, []);
 
-  console.log(imagesRow.current.scrollWidth - imagesRow.current.offsetWidth);
+  const goPrevious = (currentWidth) => {
+    setWidth(currentWidth - imagesRow.current.offsetWidth);
+    if (currentWidth === imagesRow.current.offsetWidth) {
+      setVisible(0);
+    } else {
+      setVisible(1);
+    }
+  };
+
+  const goNext = (currentWidth) => {
+    setWidth(currentWidth + imagesRow.current.offsetWidth);
+    if (
+      imagesRow.current.scrollWidth - imagesRow.current.offsetWidth <=
+      imagesRow.current.offsetWidth
+    ) {
+      setVisible(2);
+    } else {
+      setVisible(1);
+    }
+  };
 
   return (
-    <div className="row">
+    <div className="row" ref={imagesRow}>
       <h3>{title}</h3>
-      <motion.div
+      {visible !== 0 && (
+        <NavigateBeforeIcon
+          style={{ fontSize: 70 }}
+          className="navigateBefore"
+          onClick={() => goPrevious(width)}
+        />
+      )}
+      <motion.div animate={{ x: -width }} className="row__imageRow">
+        {movies.map(
+          (movie) =>
+            // To ensure all the pictures to be shown
+            ((isPosterImage && movie.poster_path) ||
+              (!isPosterImage && movie.backdrop_path)) && (
+              <motion.img
+                transition={{ duration: 0.1 }}
+                whileHover={{ scale: 1.3 }}
+                key={movie.id}
+                className={`row__image ${isPosterImage && "row__imagePoster"}`}
+                src={`${baseUrl}${
+                  isPosterImage ? movie?.poster_path : movie?.backdrop_path
+                }`}
+                alt={movie?.title || movie?.name || movie?.original_name}
+              />
+            )
+        )}
+      </motion.div>
+      {visible !== 2 && (
+        <NavigateNextIcon
+          style={{ fontSize: 70 }}
+          className="navigateNext"
+          onClick={() => goNext(width)}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Row;
+
+//  FRAMER MOTION: DRAG
+{
+  /* <motion.div
         ref={imagesRow}
         drag="x"
         dragConstraints={{ right: 0, left: -width }}
@@ -51,9 +114,5 @@ const Row = ({ title, fetchUrl, isPosterImage = false }) => {
               />
             )
         )}
-      </motion.div>
-    </div>
-  );
-};
-
-export default Row;
+      </motion.div> */
+}
